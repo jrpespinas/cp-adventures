182615fbd4a7b7a9868a1d55a483e04c1b1364d9
Bogs Espinas <jrpespinas@gmail.com>
2021-06-06 17:12:24 +0800

    feat: add new easy solution

---
+++ easy/1672.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        richest = 0
+        for account in accounts:
+            wealth = sum(account)
+            if wealth >= richest:
+                richest = wealth
+        return richest
