56c376e8f4fec9b1dff4e8b3f73016b0eac8cb01
Bogs Espinas <jrpespinas@gmail.com>
2021-06-09 22:18:54 +0800

    feat: use map function

--- jewels-and-stones/jewels-and-stones.py
+++ jewels-and-stones/jewels-and-stones.py
@@ -1,3 +1,3 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        return sum([1 for stone in stones if stone in set(jewels)])
+        return sum(map(stones.count, jewels))
