66612908f2ea4d72461c608e4028054ae7455935
Bogs Espinas <jrpespinas@gmail.com>
2021-06-09 22:20:09 +0800

    refactor: remove excessive condition

--- number-of-good-pairs/number-of-good-pairs.py
+++ number-of-good-pairs/number-of-good-pairs.py
@@ -3,8 +3,7 @@ class Solution:
         num_length = len(nums)
         good_pairs = 0
         for i in range(num_length):
-            for j in range(i, num_length):
-                if (nums[i] == nums[j]) and (i < j):
+            for j in range(i + 1, num_length):
+                if nums[i] == nums[j]:
                     good_pairs += 1
         return good_pairs
-        
