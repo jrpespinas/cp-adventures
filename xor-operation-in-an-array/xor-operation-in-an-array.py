61f86d3f365086ae6444b3afcf5933e65505428c
Bogs Espinas <38576038+jrpespinas@users.noreply.github.com>
2021-06-21 17:41:22 +0800

    chore: reimplement solution

--- xor-operation-in-an-array/xor-operation-in-an-array.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def xorOperation(self, n: int, start: int) -> int:
-        output = start
-        for i in range(1, n):
-            output ^= start + (i*2)
-        return output
