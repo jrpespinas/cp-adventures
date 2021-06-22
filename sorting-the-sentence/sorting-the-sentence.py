5d0ce1031af045a1c26fcfc97832c318c8530d49
Bogs Espinas <jrpespinas@gmail.com>
2021-06-22 19:54:51 +0800

    feat: reduce list

--- sorting-the-sentence/sorting-the-sentence.py
+++ sorting-the-sentence/sorting-the-sentence.py
@@ -5,7 +5,6 @@ class Solution:
         final = [""] * length
         order = []
         for i in range(length):
-            order.append(int(s[i][-1]) - 1)
-            final[order[i]] = s[i][:-1]
+            order = int(s[i][-1]) - 1
+            final[order] = s[i][:-1]
         return " ".join(final)
-
