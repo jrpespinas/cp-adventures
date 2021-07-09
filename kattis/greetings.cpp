012b8015310527d5231e79825adbb398845f532f
Bogs Espinas <jrpespinas@gmail.com>
2021-07-09 15:39:21 +0800

    add greetings solution

---
+++ kattis/greetings.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	int size = (str.size() - 2) * 2;	
+	cout << "h" << string(size, 'e') << "y";
+	return 0;
+}
