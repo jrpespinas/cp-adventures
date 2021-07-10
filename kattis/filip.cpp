73d71e9e7d1fbf55517ac13de436d055214a1833
Bogs Espinas <jrpespinas@gmail.com>
2021-07-10 17:46:10 +0800

    refactor: rewrite equation

--- kattis/filip.cpp
+++ kattis/filip.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int reverse(int x) {
 	int reversedNum = 0;
 	while(x != 0) {
-		reversedNum = (reversedNum * 10) + (x % 10);
+		reversedNum = (x % 10) + (reversedNum * 10);
 		x /= 10;
 	}
 	return reversedNum;
