f891600c97f44a7d20fdef82cddd03b9215c897b
Bogs Espinas <jrpespinas@gmail.com>
2021-07-27 20:53:29 +0800

    Trivial 1.3

--- kattis/icpc_awards.cpp
+++ kattis/icpc_awards.cpp
@@ -2,28 +2,27 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	int n;
-	string inst, name;
-	queue<pair<string, string>> teams;
-	unordered_map<string, int> winners;
-	cin >> n;
-	while(n--) {
-		cin >> inst >> name;
-		if (winners.count(inst))
-			continue;
-		else {
-			winners[inst] = 1;
-			teams.push({inst, name});
-		}
+    int n;
+    string inst, name;
+    queue<pair<string, string>> teams;
+    unordered_map<string, int> winners;
+    cin >> n;
+    while(n--) {
+        cin >> inst >> name;
+        if (winners.count(inst))
+            continue;
+        else {
+            winners[inst] = 1;
+            teams.push({inst, name});
+        }
 
-	}
-	cout << "\n\n\n";
-	for (int i=0; i<12; i++) {
-		cout << teams.front().first << " " << teams.front().second << "\n";
-		teams.pop();
-	}
-	return 0;
+    }
+    for (int i=0; i<12; i++) {
+        cout << teams.front().first << " " << teams.front().second << "\n";
+        teams.pop();
+    }
+    return 0;
 }
